7a874fa318fd11b53608c8960478a49cf3d3e70e
Pasha <e.hassanrazapasha@gmail.com>
2025-08-26 07:56:28 +0500

    Remove explicit registration of AccountWidget and FilamentInfoWidget in StorePanelProvider

--- app/Providers/Filament/StorePanelProvider.php
+++ app/Providers/Filament/StorePanelProvider.php
@@ -43,10 +43,6 @@ class StorePanelProvider extends PanelProvider
                 Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
-            ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
